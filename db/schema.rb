1e5decb46c716021abe1dfa833dfa8bad71871c7
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2017-10-24 15:05:35 +0000

    changes to user password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024135137) do
+ActiveRecord::Schema.define(version: 20171024150354) do
 
   create_table "teachers", force: :cascade do |t|
     t.string "photo"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20171024135137) do
     t.string "name"
     t.string "reg"
     t.boolean "admin"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
