236123c472d97e39e675c862017dd4fbba20984f
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2017-10-24 13:52:34 +0000

    adicionado professores e usuarios

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,7 @@
+class PhotoUploader < CarrierWave::Uploader::Base include Cloudinary::CarrierWave process :convert => 'png'
+ 
+  version :standard do
+    process :resize_to_fill => [100, 150, :north]
+  end
+ 
+end
