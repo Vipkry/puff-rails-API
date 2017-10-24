6dca36900c3f9f78dcf3e5ba12308a6c558c108b
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2017-10-24 14:25:44 +0000

    regex on user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    
+    before_validation :check_reg
+    
+    validates_presence_of :reg
+    
+    # OK tested
+    def check_reg
+      if !self.reg.scan(/\D/).empty?
+        self.errors.add(:reg, "Reg should be composed by digits only.") 
+      end
+    end
 end
