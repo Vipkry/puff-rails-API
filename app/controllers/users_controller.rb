7be45c7cad4c1545f4af5b080ba8ec282a1f16f6
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2018-03-13 21:19:40 -0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    @user.teacher_id = 0;
+    @user.teacher_id = 0
 
     if @user.save
       render json: @user, status: :created
